d477ec1e54a1b6336fca631dddbdd8de9d7c81c7
Ash2809 <aashutoshkumar2809@gmail.com>
2024-12-03 14:07:38 +0530

    preprocessor completed

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -5,6 +5,12 @@ import os
 
 params = yaml.safe_load(open("C:\MLOPS\Diabetes_Pred-DVC-\params.yaml"))
 
-def preprocess(input):
-    data = pd.read_csv(params['preprocessor']['input'])
+def preprocess(input, output):
+    data = pd.read_csv(input)
 
+    os.makedirs(os.path.dirname(output), exist_ok = True)
+    data.to_csv(output, header = None, index = False)
+    print(f"Preprocessed data saved to {output}")
+
+if __name__ == "__main__":
+    preprocess(params['preprocessor']['input'], params['preprocessor']['output'])
