18a22f05e0dbca2c3cfdb6d7f3d5de854d62fd84
Ash2809 <aashutoshkumar2809@gmail.com>
2024-12-03 12:45:07 +0530

    EDA Done in exp notebook

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -3,4 +3,8 @@ import sys
 import yaml 
 import os
 
-params = yaml.safe_load(open("params.yaml"))
+params = yaml.safe_load(open("C:\MLOPS\Diabetes_Pred-DVC-\params.yaml"))
+
+def preprocess(input):
+    data = pd.read_csv(params['preprocessor']['input'])
+
